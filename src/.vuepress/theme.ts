36b146253f679d3996a4e378634405b5daecd97c
nihilityer <nihilityer@mail.nihilityer.top>
2025-06-28 16:23:48 +0800

    pref: 自定义图标

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -13,7 +13,7 @@ export default hopeTheme({
 
   iconAssets: "fontawesome-with-brands",
 
-  logo: "https://theme-hope-assets.vuejs.press/logo.svg",
+  logo: "logo.svg",
 
   repo: "nihilityer/nihilityer.github.io",
 
